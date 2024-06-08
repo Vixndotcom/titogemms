be101992dc517ae3b86685b71fb8e749e0910502
alex aku <vixndotcom@gmail.com>
2024-06-08 07:51:17 +0100

    vite modified

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
+// import { build, defineConfig } from 'vite'
+import { build } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+// export default defineConfig({
+//   plugins: [react()],
+// })
+
+export default {
+  build: {
+    rollupOptions: {
+      external: ['react-router-dom']
+    }
+  }
+}
